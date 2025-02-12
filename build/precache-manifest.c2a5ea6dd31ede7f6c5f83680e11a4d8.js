972ddc078d817685237d44cad74df934798b0f2c
bmccarthy12 <147528166+bmccarthy12@users.noreply.github.com>
2025-02-12 09:01:22 -0500

    change gitignore

---
+++ build/precache-manifest.c2a5ea6dd31ede7f6c5f83680e11a4d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "923ffa7bcbab28c45843f1c5bee5822c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59c5ca43005c2066e5b7",
+    "url": "/static/css/main.c9515007.chunk.css"
+  },
+  {
+    "revision": "e96de8ce9ca193938b87",
+    "url": "/static/js/2.9da0c732.chunk.js"
+  },
+  {
+    "revision": "59c5ca43005c2066e5b7",
+    "url": "/static/js/main.2221f9ff.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
